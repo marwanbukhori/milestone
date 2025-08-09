d0a33c59945fb4e7753162ae9ec2c6f53742a203
Marwan <marwan.bukhori@silentmode.my>
2025-08-09 09:14:29 +0800

    setup: vue frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <main>
+    <h1>My Portfolio</h1>
+    <button @click="fetchProfile">Load Profile</button>
+    <pre>{{ profile }}</pre>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import axios from "axios";
+
+const profile = ref(null);
+
+async function fetchProfile() {
+  try {
+    const { data } = await axios.get(`${import.meta.env.VITE_API_URL}/profile`);
+    profile.value = data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+</script>
